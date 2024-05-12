5db22b2d1d0431eda17dae8c54f0628eccf2b32e
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-05-12 19:02:42 +0300

    test case

--- unittests/primitives/Double3Test.java
+++ unittests/primitives/Double3Test.java
@@ -4,32 +4,56 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for primitives.Double3 class
+ */
 class Double3Test {
 
+    /**
+     * Test method for {@link primitives.Double3#add(primitives.Double3)}.
+     */
     @Test
     void testAdd() {
     }
 
+    /**
+     * Test method for {@link primitives.Double3#subtract(primitives.Double3)}.
+     */
     @Test
     void testSubtract() {
     }
 
+    /**
+     * Test method for {@link primitives.Double3#scale(double)}.
+     */
     @Test
     void testScale() {
     }
 
+    /**
+     * Test method for {@link primitives.Double3#reduce(double)}.
+     */
     @Test
     void testReduce() {
     }
 
+    /**
+     * Test method for {@link primitives.Double3#product(primitives.Double3)}.
+     */
     @Test
     void testProduct() {
     }
 
+    /**
+     * Test method for {@link primitives.Double3#lowerThan(primitives.Double3)}.
+     */
     @Test
     void testLowerThan() {
     }
 
+    /**
+     * Test method for {@link primitives.Double3#lowerThan(primitives.Double3)}.
+     */
     @Test
     void testTestLowerThan() {
     }
--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -4,21 +4,52 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for primitives.Point class
+ */
 class PointTest {
 
+    /**
+     * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+     */
     @Test
     void testSubtract() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}.
+     */
     @Test
     void testAdd() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 
+    /**
+     * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
+     */
     @Test
     void testDistanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 
+    /**
+     * Test method for {@link primitives.Point#distance(primitives.Point)}.
+     */
     @Test
     void testDistance() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+
     }
 }
