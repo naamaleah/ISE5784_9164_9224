a88f061eeea15c6e3b2b2c24d0ce5d774d997143
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-05-12 11:12:00 +0300

    sync

---
+++ src/primitives/Vector.java
@@ -0,0 +1,13 @@
+package primitives;
+
+public class Vector extends Point {
+
+    @Override
+    public boolean equals (Object obj){
+        if(this== obj) return true;
+        return obj instanceof Vector other &&  super.equals(other);
+    }
+
+    @Override
+    public String toString(){ return "->" + super.toString(); }
+}
