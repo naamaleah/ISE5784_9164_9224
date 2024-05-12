771dec3dd2275646a97b8203eaaa11f80f1e6423
Yeela Molcho <yeelamolcho@gmail.com>
2024-05-12 18:45:39 +0300

    Tests

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -3,9 +3,14 @@ package geometries;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Unit tests for geometries.Plane class
+ */
 class PlaneTest {
 
+    /**
+     * Test method for {@link geometries.Plane#getNormal(primitives.Point)}.
+     */
     @Test
     void testGetNormal() {
     }
--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -3,9 +3,14 @@ package geometries;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Unit tests for geometries.Sphere class
+ */
 class SphereTest {
 
+    /**
+     * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
+     */
     @Test
     void testGetNormal() {
     }
