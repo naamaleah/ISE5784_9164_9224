b9ff21a1f98c0699fa4d9c45276863487a27dc4e
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-05-12 13:16:36 +0300

    vector

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -2,6 +2,18 @@ package primitives;
 
 public class Vector extends Point {
 
+    public Vector(double x, double y,double z) {
+        super(x,y,z);
+        if(this.xyz.equals(Double3.ZERO))
+            throw new IllegalArgumentException("Vector is zero");
+    }
+
+    public Vector(Double3 d) {
+        super(d);
+        if(d.equals(Double3.ZERO))
+            throw new IllegalArgumentException("Vector is zero");
+    }
+
     @Override
     public boolean equals (Object obj){
         if(this== obj) return true;
@@ -10,4 +22,32 @@ public class Vector extends Point {
 
     @Override
     public String toString(){ return "->" + super.toString(); }
+
+    public Vector add(Vector v){
+        return new Vector(this.xyz.add(v.xyz));
+    }
+
+    public Vector scale(double x){
+        return new Vector(this.xyz.scale(x));
+    }
+
+    public double dotProduct(Vector v){
+        return xyz.d1*v.xyz.d1+xyz.d2*v.xyz.d2+xyz.d3*v.xyz.d3;
+    }
+
+    public Vector crossProduct (Vector v){
+        return new Vector(this.xyz.d2 * v.xyz.d3 - this.xyz.d3  * v.xyz.d2,this.xyz.d3  * v.xyz.d1 - this.xyz.d1 * v.xyz.d3 ,this.xyz.d1 * v.xyz.d2 - this.xyz.d2 * v.xyz.d1)
+    }
+
+    public double lengthSquared(){
+        return dotProduct(this);
+    }
+
+    public double length(){
+        return Math.sqrt(lengthSquared());
+    }
+
+    public Vector normalize(){
+        return new Vector(this.xyz.reduce(length()));
+    }
 }
