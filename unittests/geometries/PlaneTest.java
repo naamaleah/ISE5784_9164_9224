1d3a1a86b5795072823545ff942f771f5a603d4f
Yeela Molcho <yeelamolcho@gmail.com>
2024-05-23 20:39:14 +0300

    Notes

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -61,12 +61,15 @@ class PlaneTest {
         Plane plane1 = new Plane(new Point(0,0,0), new Vector(0, 0, 1));
         Plane plane2 = new Plane(new Point(0, 0, 1), new Point(0, 1, 0), new Point(1, 0, 0));
         final var exp = List.of(new Point(0,1,0));
+        final Vector vn1n1n1=new Vector(-1,-1,-1);
+        final Vector vn050203=new Vector(-0.5,0.2,0.3);
 
         // ============ Equivalence Partitions Tests ==============
 
         // **** Group: Ray's line not orthogonal or parallel to the plane
         // TC01: Ray start after and does not crosse the plane
-        assertNull(plane1.findIntersections(new Ray(new Point(0,0,2), new Vector(0,1,1))), "Ray's line after plane");
+        assertNull(plane1.findIntersections(new Ray(new Point(0,0,2), new Vector(0,1,1))),
+                "Ray's line after plane");
         // TC02: Ray starts before and crosses the plane (1 point)
         final var result1 = plane1.findIntersections(new Ray(new Point(0,0,-1), new Vector(0,1,1)))
                 .stream().toList();
@@ -76,26 +79,30 @@ class PlaneTest {
 
         // **** Group: the ray is parallel to the plane
         // TC03: Ray is parallel to the plane and included in the plane
-        assertNull(plane2.findIntersections(new Ray(new Point(0.5, 0.25, 0.25), new Vector(-0.5, 0.2, 0.3))),
-                "ERROR: findIntersections() did not return null when the ray is parallel to the plane and included in the plane");
+        assertNull(plane2.findIntersections(new Ray(new Point(0.5, 0.25, 0.25), vn050203)),
+                "ERROR: findIntersections() did not return null when the ray is included in the plane");
 
         // TC04: ray is parallel to the plane and not included in the plane
-        assertNull(plane2.findIntersections(new Ray(new Point(0.6, 0.25, 0.25), new Vector(-0.5, 0.2, 0.3))),
-                "ERROR: findIntersections() did not return null when the ray is parallel to the plane and not included in the plane");
+        assertNull(plane2.findIntersections(new Ray(new Point(0.6, 0.25, 0.25), vn050203)),
+                "ERROR: findIntersections() did not return null when the ray is parallel " +
+                        "to the plane and not included in the plane");
 
         // **** Group: the ray is orthogonal to the plane
         // TC05: Ray is orthogonal to the plane and begins before the plane
-        final var result2 = plane2.findIntersections(new Ray(new Point(0.6, 0.25, 0.25), new Vector(-1, -1, -1)));
+        final var result2 = plane2.findIntersections(new Ray(new Point(0.6, 0.25, 0.25), vn1n1n1));
         assertEquals(1, result2.size(),
-                "ERROR: findIntersections() returned incorrect number of points when the ray is orthogonal to the plane and begins before the plane");
+                "ERROR: findIntersections() returned incorrect number of points" +
+                        " when the ray is orthogonal to the plane and begins before the plane");
 
         // TC06: Ray is orthogonal to the plane and begins in the plane
-        assertNull(plane2.findIntersections(new Ray(new Point(0.5, 0.25, 0.25), new Vector(-1, -1, -1))),
-                "ERROR: findIntersections() did not return null when the ray is orthogonal to the plane and begins in the plane");
+        assertNull(plane2.findIntersections(new Ray(new Point(0.5, 0.25, 0.25), vn1n1n1)),
+                "ERROR: findIntersections() did not return null when the ray is orthogonal to the plane and " +
+                        "begins in the plane");
 
         // TC07: Ray is orthogonal to the plane and begins after the plane
-        assertNull(plane2.findIntersections(new Ray(new Point(0.4, 0.25, 0.25), new Vector(-1, -1, -1))),
-                "ERROR: findIntersections() did not return null when the ray is orthogonal to the plane and begins after the plane");
+        assertNull(plane2.findIntersections(new Ray(new Point(0.4, 0.25, 0.25), vn1n1n1)),
+                "ERROR: findIntersections() did not return null when the ray is orthogonal " +
+                        "to the plane and begins after the plane");
 
         // **** Group: the ray is neither orthogonal nor parallel to the plane
         // TC08: Ray begins in the plane
@@ -105,6 +112,7 @@ class PlaneTest {
         // Ray is neither orthogonal nor parallel to the plane and begins in the same
         // point which appears as reference point in the plane
         assertNull(plane2.findIntersections(new Ray(new Point(0, 0, 1), new Vector(-3, 5, 2))),
-                "ERROR: findIntersections() did not return null when the ray begins in the same point which appears as reference point in the plane");
+                "ERROR: findIntersections() did not return null when the ray begins " +
+                        "in the same point which appears as reference point in the plane");
     }
 }
