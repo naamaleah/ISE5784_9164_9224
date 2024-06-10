3051a587f5bb9dfb9c126a0dac0f15b3dd9362ae
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2024-06-10 20:41:42 +0300

    notes

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -13,6 +13,7 @@ import static primitives.Util.isZero;
  * @author Naama and Yeela
  */
 public class Camera implements Cloneable {
+
     private Vector vTo;
     private Vector vUp;
     private Point p0;
@@ -29,27 +30,54 @@ public class Camera implements Cloneable {
     }
 
     /**
-     *
+     * A static method , which will return a new object of the Builder class
+     * @return new object of the Builder class
      */
     public static Builder getBuilder(){
         return new Builder();
     }
 
+    /**
+     * construct ray from a {@link Camera} towards center of a pixel in a view plane
+     * @param nX number of rows in view plane
+     * @param nY number of columns in view plane
+     * @param j column index of pixel
+     * @param i row index of pixel
+     * @return {@link Ray} from camera to center of pixel (i,j)
+     */
     public Ray constructRay(int nX, int nY, int j, int i){
+        //view plane center:
         Point Pc= p0.add(vTo.scale(distance));
+
+        // calculate "size" of each pixel -
+        // height per pixel = total "physical" height / number of rows
+        // width per pixel = total "physical" width / number of columns
         double Ry=height/nY;
         double Rx=width/nX;
+
+        // calculate necessary "size" needed to move from
+        // center of view plane to reach the middle point of pixel (i,j)
         double Yi=-(i-(nY-1)/2)*Ry;
         double Xj=(j-(nX-1)/2)*Rx;
+
+        // set result point to middle of view plane
         Point Pij=Pc;
+
+        // if result of xJ is > 0
+        // move result point left/right on  X axis
+        // to reach middle point of pixel (i,j) (on X axis direction)
         if (!isZero(Xj)) {
             Pij = Pij.add(vRight.scale(Xj));
         }
 
+        // if result of yI is > 0
+        // move result point up/down on Y axis
+        // to reach middle point of pixel (i,j)
         if (!isZero(Yi)) {
             Pij = Pij.add(vUp.scale(Yi));
         }
 
+        //return ray from camera to middle point of pixel(i,j) in view plane
         return new Ray(p0 ,Pij.subtract(p0));
     }
 
