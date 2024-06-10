4b7e7962b16f01bbc524c2be3af84a53c145cb6a
Yeela Molcho <yeelamolcho@gmail.com>
2024-06-10 21:44:34 +0300

    ImageWriterTest

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -3,6 +3,7 @@ package renderer;
 import org.junit.jupiter.api.Test;
 import primitives.Color;
 
+import static java.awt.Color.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImageWriterTest {
@@ -11,10 +12,13 @@ class ImageWriterTest {
 
     @Test
     void testWriteToImage() {
-        ImageWriter imageWriter= new ImageWriter("yellow",800,500);
+        ImageWriter imageWriter= new ImageWriter("yellowRedGrid",800,500);
         for (int i = 0; i < imageWriter.getNx(); i++)
             for (int j = 0; j < imageWriter.getNy(); j++) {
-                imageWriter.writePixel(i,j,new Color(java.awt.Color.YELLOW));
+                if(i%50 ==0 || j%50==0)
+                    imageWriter.writePixel(i,j,new Color(java.awt.Color.RED));
+                else
+                    imageWriter.writePixel(i,j,new Color(java.awt.Color.YELLOW));
 
             }
         imageWriter.writeToImage();
